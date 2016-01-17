486ee632cb71e3f54ddcd239a671f73b3998b7a4
Mike Brevoort <mike.brevoort@robotsandpencils.com>
2016-01-17 00:49:02 -0700

    Flip to server2.js

---
+++ server2.js
@@ -0,0 +1,39 @@
+var Botkit = require('botkit')
+var Witbot = require('witbot')
+
+var slackToken = process.env.SLACK_TOKEN
+var witbot = Witbot(process.env.WIT_TOKEN)
+var openWeatherApiKey = process.env.OPENWEATHER_KEY
+var weather = require('./weather')(openWeatherApiKey)
+
+var controller = Botkit.slackbot({ debug: false })
+
+controller.spawn({ token: slackToken }).startRTM(function (err, bot, payload) {
+  if (err) throw new Error('Error connecting to slack: ', err)
+  console.log('Connected to Slack')
+})
+
+controller.hears('.*', 'direct_message,direct_mention', function (bot, message) {
+  var wit = witbot.process(message.text, bot, message)
+
+  wit.hears('weather', 0.5, function (bot, message, outcome) {
+    console.log(outcome.entities.location)
+    if (!outcome.entities.location || outcome.entities.location.length === 0) {
+      bot.reply(message, 'You have to tell me *where* you want the weather')
+      return
+    }
+
+    var location = outcome.entities.location[0].value
+    weather.get(location, function (error, msg) {
+      if (error) {
+        bot.reply(message, 'Uh oh, there was a problem getting the weather. ' + error)
+        return
+      }
+      bot.reply(message, msg)
+    })
+  })
+
+  wit.otherwise(function (bot, message) {
+    bot.reply(message, 'What is this nonesense you speak!?')
+  })
+})
