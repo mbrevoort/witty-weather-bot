91fca96913608201dcab92735d4400b2f1fd8616
Mike Brevoort <mike.brevoort@robotsandpencils.com>
2016-01-19 16:32:57 -0800

    update

--- server1.js
+++ server1.js
@@ -1,7 +1,6 @@
 var Botkit = require('botkit')
 var slackToken = process.env.SLACK_TOKEN
 var weather = require('./weather')(process.env.OPENWEATHER_KEY)
-
 var controller = Botkit.slackbot({ debug: false })
 
 controller.spawn({ token: slackToken }).startRTM(function (err, bot, payload) {
@@ -15,6 +14,12 @@ controller.hears('^What is the weather in (.*)', 'direct_message,direct_mention'
     if (error) return bot.reply(message, 'Uh oh! ' + error)
     bot.reply(message, msg)
   })
+})
+
+controller.hears('^What is the magic word?', 'direct_message,direct_mention', function (bot, message) {
+  bot.reply(message, 'Please, of course.')
+  bot.reply(message, 'Who doesn\'t know that?.')
+  bot.reply(message, 'That was a rhetorical question.')
 })
 
 controller.hears('.*', 'direct_message,direct_mention', function (bot, message) {
